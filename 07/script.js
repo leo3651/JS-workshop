2d758072db511a8ef1b7948ea9177f780a76053c
Leo <leo.kowacevic@gmail.com>
2024-03-12 21:11:40 +0100

    add script.js

--- 07/script.js
+++ 07/script.js
@@ -1,12 +1,47 @@
-var v = 1;
+const strenthBarElement = document.getElementById("strength-bar");
+const inputElement = document.getElementById("password-input");
+const reasonsContainerElement = document.getElementById("reasons");
 
-var f1 = function () {
-  console.log(v);
-};
+inputElement.addEventListener("input", updateState);
 
-var f2 = function () {
-  var v = 2;
-  f1();
-};
+function updateState() {
+  const weaknesses = calcPasswordStrength(inputElement.value);
+  let strength = 100;
 
-f2();
+  reasonsContainerElement.innerHTML = "";
+  weaknesses.forEach((weakness) => {
+    // if exists
+    if (!weakness) return;
+
+    // decrease strength
+    strength -= weakness.negativePoints;
+
+    // render new messages
+    const newMessage = document.createElement("div");
+    newMessage.textContent = weakness.message;
+    reasonsContainerElement.appendChild(newMessage);
+  });
+
+  // update strength bar
+  strenthBarElement.style.setProperty("--strength-width", strength);
+}
+
+function calcPasswordStrength(password) {
+  const weaknesses = [];
+  weaknesses.push(lengthWeaknesses(password));
+
+  return weaknesses;
+}
+
+function lengthWeaknesses(password) {
+  if (password.length <= 5)
+    return {
+      message: "Password too short",
+      negativePoints: 40,
+    };
+  if (password.length <= 10)
+    return {
+      message: "Password could be longer",
+      negativePoints: 15,
+    };
+}
