4d1e40aa2e674a24038654f31c8540bd38485b83
Leo <leo.kowacevic@gmail.com>
2024-03-19 20:54:07 +0100

    styling

--- 10/script.js
+++ 10/script.js
@@ -1,2 +1,21 @@
 // import "bootstrap";
 // import "bootstrap/dist/css/bootstrap.min.css";
+const tabsContainer = document.querySelector(".list");
+const tabs = document.querySelectorAll(".list-item");
+const tabContents = document.querySelectorAll(".results");
+
+tabsContainer.addEventListener("click", (e) => {
+  const tab = e.target.closest(".list-item");
+  const tabContent = document.getElementById(
+    e.target.closest(".list-item").dataset.tabContent
+  );
+  if (!tab) return;
+
+  tabs.forEach((tab, index) => {
+    tab.classList.remove("active-tab");
+    tabContents[index].classList.remove("active-tab-content");
+  });
+
+  tab.classList.add("active-tab");
+  tabContent.classList.add("active-tab-content");
+});
