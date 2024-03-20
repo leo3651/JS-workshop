87cac3303098f1aa357d5fa565cdcb13f52f1839
Leo <leo.kowacevic@gmail.com>
2024-03-20 16:37:08 +0100

    tabs, key-value-pairs

--- 10/script.js
+++ 10/script.js
@@ -1,5 +1,8 @@
-// import "bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+//////////////////////////////
+/////// tabs functionality
+//////////////////////////////
 const tabsContainer = document.querySelector(".list");
 const tabs = document.querySelectorAll(".list-item");
 const tabContents = document.querySelectorAll(".results");
@@ -18,4 +21,51 @@ tabsContainer.addEventListener("click", (e) => {
 
   tab.classList.add("active-tab");
   tabContent.classList.add("active-tab-content");
+});
+
+//////////////////////////////
+/////// key value pairs functionality
+//////////////////////////////
+const queryParamsContent = document.getElementById("query-params-content");
+const headersContent = document.getElementById("headers-content");
+const templateKeyValue = document.querySelector("template");
+const addBtns = document.querySelectorAll(".success");
+
+createKeyValuePair(queryParamsContent);
+createKeyValuePair(headersContent);
+
+addBtns.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    createKeyValuePair(
+      e.target.closest(".active-tab-content").firstElementChild
+    );
+  })
+);
+
+function createKeyValuePair(element) {
+  const newKeyValuePair = templateKeyValue.content.cloneNode(true);
+  newKeyValuePair
+    .querySelector(".danger")
+    .addEventListener("click", (e) =>
+      e.target.closest(".key-value-pairs").remove()
+    );
+  element.append(newKeyValuePair);
+}
+
+//////////////////////////////
+/////// axios
+//////////////////////////////
+const form = document.querySelector("[data-form]");
+const url = document.querySelector("[data-url]");
+const method = document.querySelector("[data-method]");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  axios({
+    url: url.value,
+    method: method.value,
+    params: "",
+    headers: "",
+  });
 });
