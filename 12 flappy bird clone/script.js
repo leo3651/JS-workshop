4e3b2f56795a390ab82521a6785bb06e90841982
Leo <leo.kowacevic@gmail.com>
2024-03-27 23:38:52 +0100

    add pipe

--- 12 flappy bird clone/script.js
+++ 12 flappy bird clone/script.js
@@ -3,6 +3,7 @@ import { updateBird, setInitialPositionOfBird, getBirdRect } from "./bird.js";
 document.addEventListener("keypress", handleStart, { once: true });
 
 const title = document.querySelector("[data-title]");
+const subtitle = document.querySelector("[data-subtitle]");
 let lastTime;
 
 function updateFrame(time) {
@@ -19,10 +20,18 @@ function updateFrame(time) {
 function handleStart(e) {
   title.classList.add("hide");
   setInitialPositionOfBird();
+  lastTime = false;
   window.requestAnimationFrame(updateFrame);
 }
 
-function handleLose() {}
+function handleLose() {
+  subtitle.classList.remove("hide");
+  title.classList.remove("hide");
+  subtitle.textContent = `${0} Pipes`;
+  setTimeout(() => {
+    document.addEventListener("keypress", handleStart, { once: true });
+  }, 500);
+}
 
 function checkLose() {
   const birdRect = getBirdRect();
